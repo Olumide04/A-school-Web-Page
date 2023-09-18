75c212c126043709827bdf69df3128da08880b0a
Olumide Ayeromara <ayeromara12@gmail.com>
2023-09-18 15:22:52 +0100

    commit

---
+++ layout/scripts/firstbank.js
@@ -0,0 +1,39 @@
+function firstbankregistration() {
+    var firstname = document.getElementById('firstname').value;
+    document.getElementById('Tfirstname').innerHTML = firstname;
+
+    var lastname = document.getElementById('middlename').value;
+    document.getElementById('Tmiddlename').innerHTML = middlename;
+
+    var lastname = document.getElementById('lastname').value;
+    document.getElementById('Tlastname').innerHTML = lastname;
+
+    var gender = document.getElementById('gender').value;
+    document.getElementById('Tgender').innerHTML = gender;
+
+    var age = document.getElementById('age').value;
+    document.getElementById('Tage').innerHTML = age;
+
+    var lastname = document.getElementById('stateoforigin').value;
+    document.getElementById('Tstateoforigin').innerHTML = stateoforigin;
+
+    var nationality = document.getElementById('nationality').value;
+    document.getElementById('Tnationality').innerHTML = nationality;
+
+    var nationality = document.getElementById('d.o.b').value;
+    document.getElementById('Td.o.b').innerHTML = d.o.b;
+
+    var nationality = document.getElementById('address').value;
+    document.getElementById('Taddress').innerHTML = address;
+
+    var nationality = document.getElementById('telephone').value;
+    document.getElementById('Ttelephone').innerHTML = telephone;
+
+    var nationality = document.getElementById('emailaddress').value;
+    document.getElementById('Temailaddress').innerHTML = emailaddress;
+
+    var nationality = document.getElementById('typeofcurrentaccount').value;
+    document.getElementById('Ttypeofcurrentaccount').innerHTML = typeofcurrentaccount;
+
+
+}
